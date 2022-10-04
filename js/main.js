b2a76d8ab3c38e9d222335bf5650f8c5b01cac0c
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2022-10-04 21:52:47 +0300

    data generation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+const RANDOM_ADS_COUNT = 10;
+const MIN_AVATAR_SRC_NUMBER = 0;
+const MAX_AVATAR_SRC_NUMBER = 10;
+const ADS_TITLES = ['Халупа', 'Дом' , 'Пещера', 'Шале', 'Комната', 'Подвал', 'Дворец'];
+const MIN_LOCATION_LAT = 35.65000;
+const MAX_LOCATION_LAT = 35.70000;
+const MIN_LOCATION_LGN = 139.70000;
+const MAX_LOCATION_LGN = 139.80000;
+const MIN_PRICE = 10;
+const MAX_PRICE = 1000000000;
+const HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const MIN_ROOMS_COUNT = 1;
+const MAX_ROOMS_COUNT = 100;
+const MIN_GUESTS_COUNT = 1;
+const MAX_GUESTS_COUNT = 500;
+const CHECKIN_HOURS = ['12:00', '13:00', '14:00'];
+const CHECKOUT_HOURS = CHECKIN_HOURS;
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTIONS = ['Норм', 'Таксе', 'Выше всяких похвал', 'Ни то, ни сё', 'И так и сяк', 'Приличное'];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
@@ -54,3 +79,39 @@ function getRandomPositiveFloat (a, b, digits = 1) {
   // Метод возвращает строку, поэтому с помощью унарного плюса превращаем её в число
   return +result.toFixed(digits);
 }
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const createRandomAds = () => {
+  let avatarSrcNumber = getRandomPositiveInteger(MIN_AVATAR_SRC_NUMBER, MAX_AVATAR_SRC_NUMBER);
+  if(avatarSrcNumber !== MAX_AVATAR_SRC_NUMBER) {
+    avatarSrcNumber = `0${avatarSrcNumber}`;
+  }
+
+  const location = {
+    lat: getRandomPositiveFloat(MIN_LOCATION_LAT,MAX_LOCATION_LAT),
+    lng: getRandomPositiveFloat(MIN_LOCATION_LGN,MAX_LOCATION_LGN)
+  };
+
+  return {
+    author: {
+      avatar: `img/avatars/user${avatarSrcNumber}.png`,
+      offer: {
+        title: getRandomArrayElement(ADS_TITLES),
+        address: `${location.lat}, ${location.lng}`,
+        price: getRandomPositiveInteger(MIN_PRICE, MAX_PRICE),
+        type: getRandomArrayElement(HOUSING_TYPES),
+        rooms: getRandomPositiveInteger(MIN_ROOMS_COUNT, MAX_ROOMS_COUNT),
+        guests: getRandomPositiveInteger(MIN_GUESTS_COUNT, MAX_GUESTS_COUNT),
+        checkin: getRandomArrayElement(CHECKIN_HOURS),
+        checkout: getRandomArrayElement(CHECKOUT_HOURS),
+        features: FEATURES.slice(getRandomPositiveInteger(0, FEATURES.length)),
+        description: getRandomArrayElement(DESCRIPTIONS),
+        photos: PHOTOS.slice(getRandomPositiveInteger(0, PHOTOS.length)),
+      },
+      location
+    }
+  };
+};
+
+const randomAdeses = Array.from({length: RANDOM_ADS_COUNT}, createRandomAds);
