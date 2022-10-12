dc2556abdfe94a5ebc2ae7a67ea4f2368aede170
Igor Zorin <zorin.igor.jaroslavovich@gmail.com>
2022-10-13 01:02:21 +0300

    module7-task2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import { createAdElement } from './markup.js';
 import { getRandomAds } from './data.js';
+import { disableForms, enableForms } from './form.js';
 
 const adData = getRandomAds()[0];
 const adElement = createAdElement(adData);
 
 const mapCanvasElement = document.querySelector('#map-canvas');
 mapCanvasElement.append(adElement);
+
+disableForms();
+setTimeout(enableForms, 3000);
